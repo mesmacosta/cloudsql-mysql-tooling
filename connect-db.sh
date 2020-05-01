aad2c52dba66ec2bad4d623f3b875e626cf4e9d8
mesmacosta <mesmacosta@gmail.com>
2020-05-01 15:58:08 -0300

    Remove unused param

--- connect-db.sh
+++ connect-db.sh
@@ -3,10 +3,8 @@ root_dir=$(pwd)
 cd infrastructure/terraform
 
 instance_id=$(cat terraform.tfstate | jq '.outputs.instance_id.value')
-password=$(cat terraform.tfstate | jq '.outputs.password.value')
 
 # Remove quotes
-password=${password//\"/}
 instance_id=${instance_id//\"/}
 
 cd $root_dir
